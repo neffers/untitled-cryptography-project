2b95501151c4eba5693b9246cc65bf32331e6702
Jordan Brudenell <nefkrystal@gmail.com>
2023-10-14 23:02:02 -0400

    trim leaderboards list according to what user should be able to see

--- src/resource.py
+++ src/resource.py
@@ -89,10 +89,24 @@ def handle_request(request):
         user = None  # User is unregistered, a 'guest'
 
     if request_type == ResourceRequestType.ShowLeaderboards:
-        # TODO trim response based on what you should be able to see
+        if user["class"] == UserClass.Administrator:
+            leaderboards_to_return = db["leaderboards"]
+        else:
+            leaderboards_to_return = []
+            for leaderboard in db["leaderboards"]:
+                if leaderboard["visible"]:
+                    leaderboards_to_return.append(leaderboard)
+                    continue
+                # Else if leaderboard not visible by default
+                try:
+                    permission = [perm["level"] for perm in user["permission"] if perm["id"] == leaderboard["id"]][0]
+                except KeyError:
+                    permission = Permissions.NoAccess
+                if permission >= Permissions.Read:
+                    leaderboards_to_return.append(leaderboard)
         return {
             "success": True,
-            "data": db["leaderboards"]
+            "data": leaderboards_to_return
         }
 
     if request_type == ResourceRequestType.ShowOneLeaderboard:
