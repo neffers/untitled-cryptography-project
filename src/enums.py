dae1ac7ebb2107bd7e38d7f4b2a7c2b1db68c4cb
Jordan Brudenell <nefkrystal@gmail.com>
2023-10-15 21:39:17 -0400

    Add todos for remaining clientside commands to be added to resource server

--- src/enums.py
+++ src/enums.py
@@ -12,3 +12,16 @@ class ResourceRequestType(IntEnum):
     ListUsers = auto()
     ListUnverified = auto()
     GetEntry = auto()
+    # TODOS
+    # Admin: Score Order
+    # Admin: Remove Leaderboard
+    # Entry: Add Proof
+    # Entry: Download Proof
+    # Entry: Add comment
+    # Entry: Verify Entry
+    # Entry: Unverify Entry
+    # Entry: Remove Entry
+    # User: View User (get visible entries)
+    # User: View Permission
+    # User: Set Permission
+    # User: Remove User
